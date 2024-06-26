9b1174775fc814533a2da12eddac0215806e7f7e
Emma-Asoliya <e.briggs@alustudent.com>
2024-06-26 22:27:54 +0200

    update

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -14,5 +14,4 @@ def fizzbuzz():
         elif number % 5 == 0:
             print("Buzz ", end="")
         else:
-            print("{} ".format(number), end="")
-            
+            print("{} ".format(number), end="")  
