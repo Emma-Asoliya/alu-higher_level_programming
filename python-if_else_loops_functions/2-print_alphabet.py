be74e27b0b3963f8b24381a725dde721c767ea3e
Emma-Asoliya <e.briggs@alustudent.com>
2024-06-26 21:01:01 +0200

    task 2

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-import string
-print("Alphabet from a-z")
-for letter in string.ascii_lowercase:
-    print(letter, end="")
+for letter in range(97, 123):
+    print('{}'.format(chr(letter)), end='')
     
