8f928e8ff6815244dbf22e52fa28830f1ce72204
Emma-Asoliya <e.briggs@alustudent.com>
2024-07-17 00:27:43 +0200

    task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,13 +2,10 @@
 """
     define a function 'read_file'
 """
-
-
 def read_file(filename=""):
     """
         reads a text file & prints it to stdout
     """
-
     with open(filename, encoding='utf-8') as f:
         for line in f:
             print(line, end="")
