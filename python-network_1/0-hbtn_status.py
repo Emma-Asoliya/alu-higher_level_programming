9ce08b18fb36e14e58b474ab950dd701fa0be52c
Emma-Asoliya <e.briggs@alustudent.com>
2024-07-30 23:20:40 +0200

    task 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-# python script that fetches 'https://alu-intranet.hbtn.io/status'
+"""A script that
+- fetches https://alu-intranet.hbtn.io/status.
+- uses urlib package
+"""
+
 import urllib.request
 
 url = 'https://intranet.hbtn.io/status'
