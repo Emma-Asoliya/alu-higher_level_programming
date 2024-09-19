9af01bb548fa9fc30b7cb7fede2bb94db4614744
Emma-Asoliya <e.briggs@alustudent.com>
2024-09-19 21:50:06 +0200

    task 6

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const messages = ['C is fun', 'Python is cool', 'JavaScript is amazing' ];
-for (let i = 0; i < messages.length; i++) {
+const messages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-console.log(message[i]);
+for (let i = 0; i < messages.length; i++) {
+    console.log(message[i]);
 }
