d1e8a5ca1346c223ecadc8beb2465199246641d1
Emma-Asoliya <e.briggs@alustudent.com>
2024-09-19 15:59:10 +0200

    task 3

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
 const args = process.argv.slice(2);
-const firstArg = args[0];
 
-if (firstArg === undefined) {
-    console.log('No argument');
+if (args[0] === undefined) {
+  console.log('No argument');
 } else {
-    console.log(firstArg);
+  console.log(args[0]);
 }
