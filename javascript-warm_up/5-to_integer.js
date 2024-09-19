75c307c710b9bf191c07a9649adafd64cea7e974
Emma-Asoliya <e.briggs@alustudent.com>
2024-09-19 21:08:24 +0200

    task 5

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
 
-const args = progress.argv.slice(2);
+const args = process.argv.slice(2);
 
-const firstArg = parseInt(args[0]);
+const number = parseInt(args[0]);
 
 if (number === undefined) {
-    console.log('Not a number');
+  console.log('Not a number');
 } else {
-   if (isNaN(number)) {
-    console.log('Not a Number');
-   } else {
+  if (isNaN(number)) {
+    console.log('Not a number');
+  } else {
     if (typeof (number) === 'number') {
-        console.log('My number: ' + number);
+      console.log('My number: ' + number);
     }
-   }
+  }
 }
