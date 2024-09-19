056d9b0d899d14b568078d02eb3466650b83408f
Emma-Asoliya <e.briggs@alustudent.com>
2024-09-19 22:11:56 +0200

    task 8

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -7,4 +7,8 @@ if (num) {
     for (let i = 0; i < num; i++) {
         console.log('X'.repeat(num));
     }
+} else if (num < 0) {
+    console.log();
+} else {
+    console.log('Missing size');
 }
